c2673c1e84557dee88fc299780d25394b8679e36
CyberThulhu22 <37412977+CyberThulhu22@users.noreply.github.com>
2020-11-08 13:31:01 -0500

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,7 +1,7 @@
 var i = 0
 function typeWriter() {
 	// Typing Text...
-	var speed = 100; /* The Speed/Duration of the effect in milliseconds*/
+	var speed = 70; /* The Speed/Duration of the effect in milliseconds*/
 	var txt = "CyberThulhu's Main Page";
 	if (i < txt.length){
 		document.getElementById("demo").innerHTML += txt.charAt(i);
