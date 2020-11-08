f33599bdbb2aa8f950cbb757bb631007306d1b20
CyberThulhu22 <37412977+CyberThulhu22@users.noreply.github.com>
2020-11-08 14:32:54 -0500

    Added some CSS/JS

--- js/core.js
+++ js/core.js
@@ -8,4 +8,11 @@ function typeWriter() {
 		i++;
 		setTimeout(typeWriter, speed);
 	}
-}
+};
+
+function blink() {
+	var f = document.getElementById('blink');
+	setInterval( function() {
+		f.style.display = (f.style.display == 'none' ? '' : 'none');
+	}, 1000);
+};
