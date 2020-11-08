6059e72c10957a5e971d13149386ef6ed6660470
CyberThulhu22 <37412977+CyberThulhu22@users.noreply.github.com>
2020-11-08 13:21:09 -0500

    Updated JS

--- js/core.js
+++ js/core.js
@@ -1,9 +1,8 @@
 var i = 0
 function typeWriter() {
 	// Typing Text...
-	var txt = "CyberThulhu's Main Page";
 	var speed = 100; /* The Speed/Duration of the effect in milliseconds*/
-
+	var txt = "CyberThulhu's Main Page";
 	if (i < txt.length){
 		document.getElementById("demo").innerHTML += txt.charAt(i);
 		i++;
