f57ed8d05061f3eb054a2da37a6720f5281f7e89
CyberThulhu22 <37412977+CyberThulhu22@users.noreply.github.com>
2020-11-08 12:49:08 -0500

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,7 +1,7 @@
 function typeWriter(txt) {
 	// Typing Text...
 	var i = 0;
-	var speed = 50; /* The Speed/Duration of the effect in milliseconds*/
+	var speed = 30; /* The Speed/Duration of the effect in milliseconds*/
 
 	if (i < txt.length){
 		document.getElementById("demo").innerHTML += txt.charAt(i);
